db9ad235a7b0db74187f8cc3d0ff5eb11690a36a
weiyin-lu <weiyin2002@outlook.com>
2023-08-12 17:15:18 +0800

    调试门诊问诊父级页

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,23 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '../components/index/Login.vue'
 import doctorList from "@/components/backup/DoctorList.vue";
+import outDoctorMain from "@/components/outdoctor/OutDoctorMain.vue";
 
 const routes = [
+  {
+    path:'/test',
+    name:'test',
+    component: doctorList
+  },
   {
     path: '/',
     name: 'login',
     component: Login
   },
   {
-    path:'/test',
-    name:'test',
-    component: doctorList
+    path: '/outdoctor',
+    name: 'outdoctor',
+    component: outDoctorMain
   }
 ]
 
